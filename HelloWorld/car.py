079d99d7adc3072312665b56ecb2c1ee7413fc3c
sharma-adi0906 <sharma.adi0906@gmail.com>
2021-12-17 16:48:36 +0530

    test folder

---
+++ HelloWorld/car.py
@@ -0,0 +1,4 @@
+i = input("")
+if i == "santro" && "i20" && "i10" && "creta" && "venue" && "Alcazar" && "NIOS":
+    print ("Car is from Hyundai")
+elif i == "WagonR" && "Alto" && "K10" && "celerio":
